90cea7a43b85b1e2b1f183c95e9e717347e76846
Aitor <aitormz2005@gmail.com>
2025-07-31 01:20:23 +0200

    Deploy

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://restaurante-api-production-005e.up.railway.app/api',
   headers: {
     'X-Auth-Token': 'aitorpsw1234',
     "Accept": "application/json"
